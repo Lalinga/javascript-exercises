3a3f728cf94b895fb20a76f749e43bf9e0f55913
Lalinga <107472057+Lalinga@users.noreply.github.com>
2022-10-17 12:19:19 +0300

    sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
+const sumAll = function(arr) {
+ let fullArr = [];
+ let answer = 0;
+ let reducer = (sumNum, numToAdd) => sumNum + numToAdd;
 
+ arr.sort(function(a,b){return a-b})
+ for (let i = arr[0]; i <= arr[i];i++){
+   fullArr.push(i);
+ }
+ answer = fullArr.reduce(reducer);
+ return sum
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
