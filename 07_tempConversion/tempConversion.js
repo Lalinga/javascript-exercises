f763919d81c2f0212b667bb98d2784710e44e256
Lalinga <107472057+Lalinga@users.noreply.github.com>
2022-10-17 17:42:51 +0300

    tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,15 @@
-const ftoc = function() {
-
-};
-
-const ctof = function() {
-
-};
+const ftoc = function(fahrenheit) {
+  let answer = (fahrenheit - 32) * 5/9;
+  const result = answer.toFixed(1);
+  return result;
+ 
+ };
+ 
+ const ctof = function(celsius) {
+ let answer = (celsius * 9/5) + 32;
+ const result = answer.toFixed(1);
+ return result;
+ };
 
 // Do not edit below this line
 module.exports = {
